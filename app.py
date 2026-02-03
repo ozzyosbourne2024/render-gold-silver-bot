98c1bfde9d06cc5a27533a5985ce04a7e1507feb
ozzyosbourne2024 <ozeralgedik@gmail.com>
2026-02-03 19:17:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ def health():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+@app.route("/gold")
+def gold_price():
+    url = "https://api.metals.live/v1/spot"
+    data = requests.get(url, timeout=10).json()
+
+    gold = next(item[1] for item in data if item[0] == "gold")
+    return f"Altın (ons): {gold} USD"
