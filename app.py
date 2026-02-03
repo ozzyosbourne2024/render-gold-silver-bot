863e197f976fbd629e4dc8f29fb61f0bc819cce3
ozzyosbourne2024 <ozeralgedik@gmail.com>
2026-02-03 19:12:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Bot çalışıyor 🚀"
+
 @app.route("/healthz")
-def healthz():
+def health():
     return "ok", 200
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
