6c96f7d988a0ac2a160621c958bb660f37933fd2
ozzyosbourne2024 <ozeralgedik@gmail.com>
2026-02-03 18:51:43 +0300

    simplify flask app

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
@@ -7,6 +6,3 @@ app = Flask(__name__)
 def home():
     return "Bot çalışıyor 🚀"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
