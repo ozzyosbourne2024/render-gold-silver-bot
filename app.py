26e9562b6ed71e504496a4599348d901029d8557
ozzyosbourne2024 <ozeralgedik@gmail.com>
2026-02-03 19:22:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import requests
-
 from flask import Flask
 import os
 
@@ -9,6 +8,13 @@ app = Flask(__name__)
 def home():
     return "Bot çalışıyor 🚀"
 
+@app.route("/gold")
+def gold_price():
+    url = "https://api.metals.live/v1/spot"
+    data = requests.get(url, timeout=10).json()
+    gold = next(item[1] for item in data if item[0] == "gold")
+    return f"Altın (ons): {gold} USD"
+
 @app.route("/healthz")
 def health():
     return "ok", 200
@@ -16,10 +22,3 @@ def health():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-@app.route("/gold")
-def gold_price():
-    url = "https://api.metals.live/v1/spot"
-    data = requests.get(url, timeout=10).json()
-
-    gold = next(item[1] for item in data if item[0] == "gold")
-    return f"Altın (ons): {gold} USD"
