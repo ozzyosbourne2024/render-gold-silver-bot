05aaeabffc5ce216bff373b4737ccd199e5b7a8b
ozzyosbourne2024 <ozeralgedik@gmail.com>
2026-02-03 18:19:03 +0300

    fix render port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,5 @@ def home():
     return "Bot çalışıyor 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
